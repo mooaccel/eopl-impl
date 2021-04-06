a81ba0fc623fd0e8dd1e7be3bb7658db220c8591
mojiajun <mojiajunmail@gmail.com>
2021-04-06 14:03:43 +0800

    add 1_21, using flatmap

--- ch1/1_21.scm
+++ ch1/1_21.scm
@@ -10,3 +10,18 @@
          sos2)))
 (product '(a b c) '(x y))
 (product '(a b c) '(1 2 3))
+
+(define (accumulate op initial sequence) 
+  (if (null? sequence) 
+      initial 
+      (op (car sequence) 
+          (accumulate op initial (cdr sequence)))))
+(define (flatmap proc seq) 
+  (accumulate append '() (map proc seq)))
+(define product-v2
+  (lambda (sos1 sos2)
+    (flatmap (lambda (s1)
+              (helper s1 sos2))
+             sos1)))
+(product-v2 '(a b c) '(x y))
+(product-v2 '(a b c) '(1 2 3))
