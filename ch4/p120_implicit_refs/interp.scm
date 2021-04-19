f8d557627e9b52e632fb7e8742ee61758e7ed5c8
mooaccel <mooaccel@gmail.com>
2021-04-19 23:07:34 +0800

    添加IMPLICIT-REFS书本例子, 下一步做习题

---
+++ ch4/p120_implicit_refs/interp.scm
@@ -0,0 +1,141 @@
+(module interp (lib "eopl")
+  
+  (require "drscheme-init.scm")
+
+  (require "lang.scm")
+  (require "data-structures.scm")
+  (require "environments.scm")
+  (require "store.scm")  ; initialize-store!
+  
+  (provide value-of-program value-of instrument_let instrument_newref)  
+  ; instrument_newref在store.scm定义, 可以再更上层top.scm使用, 在中间这一层用provide
+
+  (define instrument_let (make-parameter #f))
+  ;; say (instrument_let #t) to turn instrumentation on.
+  ;;     (instrument_let #f) to turn it off again.
+
+
+;;;;;;;;;;;;;;;; the interpreter ;;;;;;;;;;;;;;;;
+  ;; value-of-program : Program -> ExpVal
+  ;; Page: 110
+  (define value-of-program 
+    (lambda (pgm)
+      (initialize-store!)               ; new for explicit refs.
+      (cases program pgm
+        (a-program (exp1)
+          (value-of exp1 (init-env))))))
+
+  ;; value-of : Exp * Env -> ExpVal
+  ;; Page: 113
+  (define value-of
+    (lambda (exp env)
+      (cases expression exp
+
+        (const-exp (num) (num-val num))
+
+        (var-exp (var)         ; var-exp和Explicit Refs语言有很大不同
+          (deref (apply-env env var)))
+
+        (diff-exp (exp1 exp2)
+          (let ((val1 (value-of exp1 env))
+                (val2 (value-of exp2 env)))
+            (let ((num1 (expval->num val1))
+                  (num2 (expval->num val2)))
+              (num-val
+                (- num1 num2)))))
+      
+        (zero?-exp (exp1)
+          (let ((val1 (value-of exp1 env)))
+            (let ((num1 (expval->num val1)))
+              (if (zero? num1)
+                (bool-val #t)
+                (bool-val #f)))))
+              
+        (if-exp (exp1 exp2 exp3)
+          (let ((val1 (value-of exp1 env)))
+            (if (expval->bool val1)
+              (value-of exp2 env)
+              (value-of exp3 env))))
+
+        (let-exp (var exp1 body)       
+          (let ((val1 (value-of exp1 env)))
+            (value-of body
+              (extend-env var (newref val1) env))))  ; 现在env的val存的都是ref
+        
+        (proc-exp (var body)
+          (proc-val (procedure var body env)))
+
+        (call-exp (rator rand)
+          (let ((proc (expval->proc (value-of rator env)))
+                (arg (value-of rand env)))
+            (apply-procedure proc arg)))
+
+        (letrec-exp (p-names b-vars p-bodies letrec-body)
+          (value-of letrec-body
+            (extend-env-rec* p-names b-vars p-bodies env)))
+
+        (begin-exp (exp1 exps)
+          (letrec 
+            ((value-of-begins
+               (lambda (e1 es)
+                 (let ((v1 (value-of e1 env)))
+                   (if (null? es)
+                     v1
+                     (value-of-begins (car es) (cdr es)))))))
+            (value-of-begins exp1 exps)))
+
+        ; (assign-exp (ident exp1)
+        ;   (begin
+        ;     (let ((val_of_exp1 (value-of exp1 env)))
+        ;       (setref! (apply-env env ident) 
+        ;                val_of_exp1))
+        ;     (num-val 34)))
+        ; 下面这样也可以...
+        (assign-exp (ident exp1)
+            (let ((val_of_exp1 (value-of exp1 env)))
+              (setref! (apply-env env ident) 
+                       val_of_exp1)))
+            
+        )))
+
+  ;; apply-procedure : Proc * ExpVal -> ExpVal
+  ;; 
+  ;; uninstrumented version
+  ;;   (define apply-procedure
+  ;;    (lambda (proc1 arg)
+  ;;      (cases proc proc1
+  ;;        (procedure (bvar body saved-env)
+  ;;          (value-of body (extend-env bvar arg saved-env))))))
+
+  ;; instrumented version
+  (define apply-procedure
+    (lambda (proc1 val)
+      (cases proc proc1
+        (procedure (var body saved-env)
+      	    (let ((new-env (extend-env var (newref val) saved-env)))
+      	      (when (instrument_let)
+      		          (begin
+      		            (eopl:printf
+      		              "entering body of proc ~s with env =~%"
+      		              var)
+      		            (pretty-print (env->list new-env))
+                      (eopl:printf "store =~%")
+                      (pretty-print (store->readable (get-store-as-list)))
+                      (eopl:printf "~%")))
+              (value-of body new-env))))))
+
+
+  ;; store->readable : Listof(List(Ref,Expval)) 
+  ;;                    -> Listof(List(Ref,Something-Readable))
+  (define store->readable
+    (lambda (l)
+      (map
+        (lambda (p)
+          (cons
+            (car p)
+            (expval->printable (cadr p))))
+        l)))
+ 
+  )
+
+
--- summary/ch4.md
+++ summary/ch4.md
@@ -3,3 +3,8 @@
 
 ## 4.2 EXPLICIT-REFS: A Language with Explicit References
 用EXPLICIT-REFS举例子. 参考ch4/p111_lang_with_explicit_ref/p111_lang_with_explicit_ref.scm
+
+## 4.3
+two-level
+
+ident(var) -> ref(location) -> val(expval)
