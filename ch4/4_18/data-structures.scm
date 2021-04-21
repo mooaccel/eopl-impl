9bbe6ced9c8d081be2180069bcc72ac5efcfac6f
mooaccel <mooaccel@gmail.com>
2021-04-21 17:30:13 +0800

    4_18 实现letrec

---
+++ ch4/4_18/data-structures.scm
@@ -0,0 +1,92 @@
+(module data-structures (lib "eopl")
+
+  (require "lang.scm")                  ; for expression?
+  (require "store.scm")                 ; for reference?
+
+  (provide (all-defined-out))               ; too many things to list
+
+  ;;;;;;;;;;;;;;;; expressed values ;;;;;;;;;;;;;;;;
+  (define-datatype expval expval?
+    (num-val
+      (value number?))
+    (bool-val
+      (boolean boolean?))
+    (proc-val 
+      (proc proc?))
+    )
+
+  ;;; extractors:
+  (define expval->num
+    (lambda (v)
+      (cases expval v
+	(num-val (num) num)
+	(else (expval-extractor-error 'num v)))))
+
+  (define expval->bool
+    (lambda (v)
+      (cases expval v
+	(bool-val (bool) bool)
+	(else (expval-extractor-error 'bool v)))))
+
+  (define expval->proc
+    (lambda (v)
+      (cases expval v
+	(proc-val (proc) proc)
+	(else (expval-extractor-error 'proc v)))))
+
+  (define expval-extractor-error
+    (lambda (variant value)
+      (eopl:error 'expval-extractors "Looking for a ~s, found ~s"
+                  variant value)))
+
+
+;;;;;;;;;;;;;;;; procedures ;;;;;;;;;;;;;;;;
+  (define-datatype proc proc?
+    (procedure
+      (vars (list-of symbol?))
+      (body expression?)
+      (env environment?)))  ; 创建proc时保存当时的env(如果想优化参考3_26)
+  
+  (define-datatype environment environment?
+    (empty-env)
+    (extend-env 
+      (vars (list-of symbol?))
+      (val_refs (list-of reference?))       ; env保存的是ref
+      (saved-env environment?))
+    (extend-env-rec
+      (listof_proc_name (list-of identifier?))
+      (listof_bound_vars (list-of (list-of identifier?)))
+      (listof_proc_body (list-of expression?))
+      (saved-env environment?))
+  )
+
+  ;; env->list : Env -> List
+  ;; used for pretty-printing and debugging
+  (define env->list
+    (lambda (env)
+      (cases environment env
+	      (empty-env () '())
+	      (extend-env (vars val_refs saved-env)
+	        (cons
+	          (list 'vars: vars 'val_refs: val_refs)  ; 为了可读性.
+	          (env->list saved-env)))
+        (extend-env-rec (listof_proc_name listof_bound_vars listof_proc_body saved_env)
+	        (cons
+	          (list 'letrec listof_proc_name '...)
+	          (env->list saved_env)))
+      )))
+
+  ;; expval->printable : ExpVal -> List
+  ;; returns a value like its argument, except procedures get cleaned
+  ;; up with env->list 
+  (define expval->printable
+    (lambda (val)
+      (cases expval val
+	      (proc-val (p)
+	        (cases proc p
+	          (procedure (vars body saved-env)
+	            (list 'procedure vars 'body... 'saved_env: (env->list saved-env)))))
+	      (else val))))
+
+
+)
---
+++ ch4/4_18/interp.scm
@@ -0,0 +1,155 @@
+(module interp (lib "eopl")
+  
+  (require "drscheme-init.scm")
+
+  (require "lang.scm")
+  (require "data-structures.scm")
+  (require "environments.scm")
+  (require "store.scm")  ; initialize-store!
+  
+  (provide value-of-program value-of instrument_let instrument_newref)  
+  ; instrument_newref在store.scm定义, 可以再更上层top.scm使用, 在中间这一层用provide
+
+  (define instrument_let (make-parameter #f))
+  ;; say (instrument_let #t) to turn instrumentation on.
+  ;;     (instrument_let #f) to turn it off again.
+
+
+;;;;;;;;;;;;;;;; the interpreter ;;;;;;;;;;;;;;;;
+  ;; value-of-program : Program -> ExpVal
+  ;; Page: 110
+  (define value-of-program 
+    (lambda (pgm)
+      (initialize-store!)               ; new for explicit refs.
+      (cases program pgm
+        (a-program (exp1)
+          (value-of exp1 (init-env))))))
+
+  ;; value-of : Exp * Env -> ExpVal
+  ;; Page: 113
+  (define value-of
+    (lambda (exp env)
+      (cases expression exp
+
+        (const-exp (num) (num-val num))
+
+        (var-exp (var)         ; var-exp和Explicit Refs语言有很大不同
+          (deref (apply-env env var)))
+
+        (diff-exp (exp1 exp2)
+          (let ((val1 (value-of exp1 env))
+                (val2 (value-of exp2 env)))
+            (let ((num1 (expval->num val1))
+                  (num2 (expval->num val2)))
+              (num-val
+                (- num1 num2)))))
+      
+        (zero?-exp (exp1)
+          (let ((val1 (value-of exp1 env)))
+            (let ((num1 (expval->num val1)))
+              (if (zero? num1)
+                (bool-val #t)
+                (bool-val #f)))))
+              
+        (if-exp (exp1 exp2 exp3)
+          (let ((val1 (value-of exp1 env)))
+            (if (expval->bool val1)
+              (value-of exp2 env)
+              (value-of exp3 env))))
+
+        (let-exp (vars exps body)       
+          (let ((vals_of_exps (map (lambda (exp_item) 
+                                      (value-of exp_item env))
+                                   exps)))
+            (value-of body
+              (extend-env vars 
+                          (map (lambda (val_item) 
+                                  (newref val_item))
+                               vals_of_exps)
+                          env))))
+
+        (proc-exp (vars body)
+          (proc-val (procedure vars body env)))
+
+        (call-exp (rator rands)
+          (let ((proc (expval->proc (value-of rator env)))
+                (args (map (lambda (rand)
+                              (value-of rand env))
+                           rands)))
+            (apply-procedure proc args)))
+
+        (letrec-exp (listof_proc_name listof_bound_vars listof_proc_body letrec_body)
+          (value-of letrec_body
+            (extend-env-rec listof_proc_name listof_bound_vars listof_proc_body env)))
+
+        (begin-exp (exp1 exps)
+          (letrec 
+            ((value-of-begins
+               (lambda (e1 es)
+                 (let ((v1 (value-of e1 env)))
+                   (if (null? es)
+                     v1
+                     (value-of-begins (car es) (cdr es)))))))
+            (value-of-begins exp1 exps)))
+
+        ; (assign-exp (ident exp1)
+        ;   (begin
+        ;     (let ((val_of_exp1 (value-of exp1 env)))
+        ;       (setref! (apply-env env ident) 
+        ;                val_of_exp1))
+        ;     (num-val 34)))
+        ; 下面这样也可以...
+        (assign-exp (ident exp1)
+            (let ((val_of_exp1 (value-of exp1 env)))
+              (setref! (apply-env env ident) 
+                       val_of_exp1)))
+            
+        )))
+
+  ;; apply-procedure : Proc * ExpVal -> ExpVal
+  ;; uninstrumented version
+  ;;   (define apply-procedure
+  ;;    (lambda (proc1 arg)
+  ;;      (cases proc proc1
+  ;;        (procedure (bvar body saved-env)
+  ;;          (value-of body (extend-env bvar arg saved-env))))))
+  ;; instrumented version
+  (define apply-procedure
+    (lambda (proc1 vals)
+      (cases proc proc1
+        (procedure (vars body saved-env)
+      	    (let ((new_env (extend-env vars
+                                       (map (lambda (val)
+                                                (newref val))
+                                            vals) 
+                                       saved-env)))
+              ; 在apply-procedure内部debug输出
+      	      (when (instrument_let)
+      		          (begin
+      		            (eopl:printf
+      		              "entering body of proc ~s with env =\n"
+      		              vars)
+      		            (pretty-print (env->list new_env))
+                      (eopl:printf "store =\n")
+                      (pretty-print (store->readable (get-store-as-list)))
+                      (eopl:printf "\n")
+                      ))
+              (value-of body new_env))))))
+
+
+  ;; store->readable : Listof(List(Ref,Expval)) 
+  ;;                    -> Listof(List(Ref,Something-Readable))
+  (define store->readable
+    (lambda (l)
+      (map
+        (lambda (p)
+          (list
+            'ref:
+            (car p)
+            'content_of_ref:
+            (expval->printable (cadr p))))
+        l)))
+ 
+  )
+
+
