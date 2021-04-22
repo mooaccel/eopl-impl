b2f51c5af6ae64855c156ea844bfe61c66d17154
mooaccel <mooaccel@gmail.com>
2021-04-22 10:57:21 +0800

    add 4_21

---
+++ ch4/4_21/tests.scm
@@ -0,0 +1,81 @@
+(module tests mzscheme
+  
+  (provide test-list)
+  ;;;;;;;;;;;;;;;; tests ;;;;;;;;;;;;;;;;
+  
+  (define test-list
+    '(
+
+      (let-multi-arg
+        "
+        let a = 100
+            b = 200
+            c = 300
+        in let f = proc (x)
+                    begin
+                      set a = x;
+                      -(a, c)
+                    end
+           in (f 20)
+        "
+        -280)
+
+      (proc-multi-arg
+        "
+        let f = proc (x y) -(x, y)
+        in (f 3 -4)
+        "
+        7)
+
+      (letrec-test-case-01
+        "
+        letrec even(x) = if zero?(x) then 1 else (odd -(x,1)) 
+               odd(x) = if zero?(x) then 0 else (even -(x,1)) 
+        in (odd 5)
+        "
+        1)
+      (letrec-test-case-02
+        "
+        letrec even(x) = if zero?(x) then 1 else (odd -(x,1)) 
+               odd(x) = if zero?(x) then 0 else (even -(x,1)) 
+        in (even 4)
+        "
+        1)
+      (setdynamic-test-case-01
+        " 
+        let x = 11
+            in let p = proc (y) -(y,x)
+               in -(setdynamic x = 17 during (p 22), 
+                    (p 13))
+        "
+        3
+      )
+
+    ))
+)
+
+  ; (eopl:pretty-print 
+  ;   (run "let f = proc (x) 
+  ;                   proc (y) 
+  ;                     begin 
+  ;                       set x = -(x,-1); 
+  ;                       -(x,y) 
+  ;                     end
+  ;         in ((f 44) 33)")
+  ; )
+
+  ; (run-one 'let-multiarg)
+
+  ; (eopl:pretty-print (run
+  ; "
+  ; let f = proc (x, y) +(x,y)
+  ; in (f 3 4)
+  ; "
+  ; ))
+  ; (eopl:pretty-print (run
+  ; "
+  ; letrec even(x) = if zero?(x) then 1 else (odd -(x,1)) 
+  ;        odd(x) = if zero?(x) then 0 else (even -(x,1)) 
+  ; in (odd 13)
+  ; "
+  ; ))
