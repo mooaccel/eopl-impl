91d7b2df2e3f168a818c0581be5579bb0969e274
mooaccel <mooaccel@gmail.com>
2021-04-27 16:07:55 +0800

    add 5_13, 分析todo

---
+++ ch5/5_13/lang.scm
@@ -0,0 +1,94 @@
+(module lang (lib "eopl.ss" "eopl")                
+
+  ;; grammar for the LETREC language
+
+  (require "drscheme-init.scm")
+  
+  (provide (all-defined-out))
+
+  ;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+  
+  (define the-lexical-spec
+    '((whitespace (whitespace) skip)
+      (comment ("%" (arbno (not #\newline))) skip)
+      (identifier
+       (letter (arbno (or letter digit "_" "-" "?")))
+       symbol)
+      (number (digit (arbno digit)) number)
+      (number ("-" digit (arbno digit)) number)
+      ))
+  
+  (define the-grammar
+    '((program (expression) a-program)
+
+      (expression (number) const-exp)
+
+      (expression (identifier) var-exp)
+
+      (expression
+        ("-" "(" expression "," expression ")")
+        diff-exp)
+      (expression
+        ("*" "(" expression "," expression ")")
+        multiplication-exp)
+      
+      (expression
+       ("zero?" "(" expression ")")
+       zero?-exp)
+
+      (expression
+       ("if" expression "then" expression "else" expression)
+       if-exp)
+
+      ;(expression
+      ; ("let" identifier "=" expression "in" expression)
+      ; let-exp)   
+
+      (expression
+       ("let2" identifier "=" expression 
+               identifier "=" expression
+        "in" expression)
+       let2-exp)   
+
+      (expression ("let" (arbno identifier "=" expression) "in" expression) let-exp)
+
+      ; (expression
+      ;  ("proc" "(" identifier ")" expression)
+      ;  proc-exp)
+
+      ; (expression
+      ;  ("(" expression expression ")")
+      ;  call-exp)
+      (expression ("proc" "(" (separated-list identifier ",") ")" expression) proc-exp)  ; proc也可以没有参数
+      (expression ("(" expression (arbno expression) ")") call-exp)  ; (f)可以没有参数, 只有rator
+
+      (expression
+        ("letrec"
+          identifier "(" (separated-list identifier ",") ")" "=" expression
+          "in" expression)
+        letrec-exp)
+
+      (expression ("cons" "(" expression "," expression ")") cons-exp)
+      (expression ("car" "(" expression ")") car-exp)
+      (expression ("cdr" "(" expression ")") cdr-exp)
+      (expression ("emptylist") emptylist-exp)
+      (expression ("null?" "(" expression ")") null?-exp)
+
+      (expression ("list" "(" (separated-list expression ",") ")" ) list-exp)
+      
+      ))
+  
+  ;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+  
+  (sllgen:make-define-datatypes the-lexical-spec the-grammar)
+  
+  (define show-the-datatypes
+    (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+  
+  (define scan&parse
+    (sllgen:make-string-parser the-lexical-spec the-grammar))
+  
+  (define just-scan
+    (sllgen:make-string-scanner the-lexical-spec the-grammar))
+  
+  )
---
+++ ch5/5_13/top.scm
@@ -0,0 +1,87 @@
+(module top (lib "eopl.ss" "eopl")
+  
+  ;; top level module.  Loads all required pieces.
+  ;; Run the test suite with (run-all).
+
+  (require "drscheme-init.scm")
+  (require "data-structures.scm")  ; for expval constructors
+  (require "lang.scm")             ; for scan&parse
+  (require "interp.scm")           ; for value-of-program
+  (require "tests.scm")            ; for test-list
+  (require "environments.scm")     ; for empty-env
+  
+  ;; since this is the top-level module, we don't really need to
+  ;; provide anything, but we do so just in case.  
+
+  (provide run run-all)
+  
+  ;;; interface for book test ;;;
+  (provide test-all)
+  (define (test-all) (run-all))
+
+  ;;;;;;;;;;;;;;;; interface to test harness ;;;;;;;;;;;;;;;;
+  
+  ;; run : String -> ExpVal
+  (define run
+    (lambda (string)
+      (value-of-program (scan&parse string))))
+  
+  ;; run-all : () -> Unspecified
+
+  ;; runs all the tests in test-list, comparing the results with
+  ;; equal-answer?  
+
+  (define run-all
+    (lambda ()
+      (run-tests! run equal-answer? test-list)))
+  
+  (define equal-answer?
+    (lambda (ans correct-ans)
+      (equal? ans (sloppy->expval correct-ans))))
+  
+  (define sloppy->expval 
+    (lambda (sloppy-val)
+      (cond
+        ((number? sloppy-val) (num-val sloppy-val))
+        ((boolean? sloppy-val) (bool-val sloppy-val))
+        (else
+         (eopl:error 'sloppy->expval 
+                     "Can't convert sloppy value to expval: ~s"
+                     sloppy-val)))))
+    
+  ;; run-one : Sym -> ExpVal
+
+  ;; (run-one sym) runs the test whose name is sym
+  
+  (define run-one
+    (lambda (test-name)
+      (let ((the-test (assoc test-name test-list)))
+        (cond
+          ((assoc test-name test-list)
+           => (lambda (test)
+                (run (cadr test))))
+          (else (eopl:error 'run-one "no such test: ~s" test-name))))))
+
+   ;; ===================
+ 
+  ; let+proc不能递归, 所以得用letrec
+  ;(eopl:pretty-print (run
+  ;" 
+  ;letrec fact(x) = if zero?(x) 
+  ;                 then 1
+  ;                 else *(x, (fact -(x, 1)))
+  ;in (fact 4)
+  ;"
+  ;))
+  (eopl:pretty-print (run
+  " 
+  letrec factiteracc(n, acc) = 
+                   if zero?(n) 
+                   then acc
+                   else (factiteracc -(n, 1) *(acc, n))
+  in let factiter = proc(n) (factiteracc n 1)
+     in (factiter 4)
+  " 
+  ))
+
+  )
